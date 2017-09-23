47f434c79eda85764c25e4a83fa5ae8bd8d78ba7
Adam Buckley <adambuckley@live.com>
2017-09-23 10:19:58 +0100

    changes made,

--- src/lab2testing/MyGui.java
+++ src/lab2testing/MyGui.java
@@ -53,20 +53,108 @@ public class MyGui extends JFrame {
 	private final String dbName = "test2_create_db";
 
 	/** The name of the table we are testing with */
-	private final String tableName = "JDBC_TEST7";
+	private final String tableName = "JDBC_TEST9";
+
+	/**
+	 * Get a new database connection
+	 * 
+	 * @return
+	 * @throws SQLException
+	 */
+	public Connection getConnection() throws SQLException {
+		Connection conn = null;
+		Properties connectionProps = new Properties();
+		connectionProps.put("user", this.userName);
+		connectionProps.put("password", this.password);
+
+		conn = DriverManager.getConnection("jdbc:mysql://"
+				+ this.serverName + ":" + this.portNumber + "/" + this.dbName,
+				connectionProps);
+
+		return conn;
+	}
+
+	/**
+	 * Run a SQL command which does not return a recordset:
+	 * CREATE/INSERT/UPDATE/DELETE/DROP/etc.
+	 * 
+	 * @throws SQLException If something goes wrong
+	 */
+	public boolean executeUpdate(Connection conn, String command) throws SQLException {
+		Statement stmt = null;
+		try {
+			stmt = conn.createStatement();
+			stmt.executeUpdate(command); // This will throw a SQLException if it fails
+			return true;
+		} finally {
+
+			// This will run whether we throw an exception or not
+			if (stmt != null) { stmt.close(); }
+		}
+	}
+
+	/**
+	 * Connect to MySQL and do some stuff.
+	 */
+	public void run() {
+
+		// Connect to MySQL
+		Connection conn = null;
+		try {
+			System.out.println("got here 1");
+			conn = this.getConnection();
+			System.out.println("Connected to database");
+		} catch (SQLException e) {
+			System.out.println("ERROR: Could not connect to the database");
+			e.printStackTrace();
+			return;
+		}
+
+		//		// Create a table
+				try {
+					System.out.println("got here");
+					String createString =
+							"CREATE TABLE " + this.tableName + " ( " +
+									"ID INTEGER NOT NULL, " +
+									"NAME varchar(40) NOT NULL, " +
+									"STREET varchar(40) NOT NULL, " +
+									"CITY varchar(20) NOT NULL, " +
+									"STATE char(2) NOT NULL, " +
+									"ZIP char(5), " +
+									"PRIMARY KEY (ID))";
+					this.executeUpdate(conn, createString);
+					System.out.println("Created a table");
+				} catch (SQLException e) {
+					System.out.println("ERROR: Could not create the table");
+					e.printStackTrace();
+					return;
+				}
+
+		//		// Drop the table
+		//		try {
+		//			String dropString = "DROP TABLE " + this.tableName;
+		//			this.executeUpdate(conn, dropString);
+		//			System.out.println("Dropped the table");
+		//		} catch (SQLException e) {
+		//			System.out.println("ERROR: Could not drop the table");
+		//			e.printStackTrace();
+		//			return;
+		//		}
+	}
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		
-		JTextField textField = new JTextField(20);
+	
+		MyGui app = new MyGui();
+		app.run();
 		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MyGui frame = new MyGui();
-					frame.setVisible(true);
+					//MyGui frame = new MyGui();
+					app.setVisible(true);
 					
 				} catch (Exception e) {
 					e.printStackTrace();
